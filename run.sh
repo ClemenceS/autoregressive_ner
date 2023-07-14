27b7e972f5a9de29a985c4c2e1a60188f0a93d9a
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-07-14 17:36:49 +0200

    fix

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10G
-#SBATCH --time=10:00:00
+#SBATCH --time=40:00:00
 #SBATCH --gres=gpu:1
 
 export http_proxy=http://webproxy.lab-ia.fr:8080
@@ -15,4 +15,4 @@ export https_proxy=http://webproxy.lab-ia.fr:8080
 export HTTP_PROXY=http://webproxy.lab-ia.fr:8080
 export HTTPS_PROXY=http://webproxy.lab-ia.fr:8080
 
-python3 bloom_ner.py
+python3 bloom_ner.py --domain clinical --top_p 0.4 0.5 0.6 --top_k 7 8 9 --temperature 0.7 0.8 0.9
