17341b9df559aab276721d50258ab75b308592da
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-08-31 17:30:02 +0200

    dont bother

--- bloom_ner.py
+++ bloom_ner.py
@@ -233,7 +233,6 @@ for n_few_shot, random_seed in itertools.product(args.n_few_shot, args.random_se
                 n_few_shot,
                 args.criterion,
                 self_verification=args.self_verification,
-                sticked=True,
                 keywords=prompt_keywords[args.prompt_dict]
             )
             prompts += k_prompts
--- prompt_maker.py
+++ prompt_maker.py
@@ -3,7 +3,7 @@ import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def example2string(example, ner_tag_id, begin_tag, end_tag, sticked, tagged=True):
+def example2string(example, ner_tag_id, begin_tag, end_tag, sticked=True, tagged=True):
     # if ner_tag_id = 3 and 3 stands for LOC, beginning tag = @@ and ending tag = ##
     # and the example is {'id': 0, 'words': ['I', 'love', 'Paris', 'and', 'Berlin'], 'ner_tags': [0, 0, 3, 0, 3]}
     # the returned string will be 'I love @@Paris## and @@Berlin##'
@@ -23,12 +23,12 @@ def example2string(example, ner_tag_id, begin_tag, end_tag, sticked, tagged=True
     return string.strip()
     
 
-def make_prompts(train_dataset, test_dataset, ner_tag, ner_tag_id, domain, begin_tag, end_tag, n_few_shots, criterion, self_verification, keywords, sticked=True):
+def make_prompts(train_dataset, test_dataset, ner_tag, ner_tag_id, domain, begin_tag, end_tag, n_few_shots, criterion, self_verification, keywords):
     #this function takes an example and a ner tag and returns a prompt in english
     few_shots_for_all = []
     num_prompts = len(test_dataset)
     def sentences_with_most_occurences(train_dataset, ner_tag_id, n):
-        strings = [example2string(e, ner_tag_id, begin_tag, end_tag, tagged=True, sticked=sticked) for e in train_dataset]
+        strings = [example2string(e, ner_tag_id, begin_tag, end_tag, tagged=True) for e in train_dataset]
         begin_tag_regex = re.escape(begin_tag)
         end_tag_regex = re.escape(end_tag)
         entities = [re.findall(begin_tag_regex+'(.*?)'+end_tag_regex, s) for s in strings]
