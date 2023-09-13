212d4b26d0dc3b103d58bdc01f828e523690c2b9
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-09-13 17:52:15 +0200

    feat : faster generation (check 347138 and 347009)

--- bloom_predict.py
+++ bloom_predict.py
@@ -6,17 +6,31 @@ from sklearn.model_selection import KFold
 from prompt_maker import make_prompts, get_yes_no_words
 from transformers import StoppingCriteria
 
-def get_prompt_for_model(model_name, prompt):
-    if 'vicuna' in model_name:
-        conv = get_conversation_template(model_name)
-        conv.append_message(conv.roles[0], prompt)
-        conv.append_message(conv.roles[1], None)
-        prompt = conv.get_prompt()
-    elif 'bloom' in model_name:    
-        pass
-    else:
-        raise NotImplementedError("Model not supported")
-    return prompt
+def get_prompt_for_model(model_name, prompts):
+    # if 'vicuna' in model_name:
+    #     conv = get_conversation_template(model_name)
+    #     conv.append_message(conv.roles[0], prompt)
+    #     conv.append_message(conv.roles[1], None)
+    #     prompt = conv.get_prompt()
+    # elif 'bloom' in model_name:    
+    #     pass
+    # else:
+    #     raise NotImplementedError("Model not supported")
+    # return prompt
+    new_prompts = []
+    for prompt in prompts:
+        if 'vicuna' in model_name:
+            conv = get_conversation_template(model_name)
+            conv.append_message(conv.roles[0], prompt)
+            conv.append_message(conv.roles[1], None)
+            prompt = conv.get_prompt()
+        elif 'bloom' in model_name:    
+            pass
+        else:
+            raise NotImplementedError("Model not supported")
+        new_prompts.append(prompt)
+    return new_prompts
+        
 
 def validate_sentence(s, begin_tag, end_tag):
     return (
@@ -102,15 +116,25 @@ def bloom_predict(training_data, testing_data, ner_tags, model_name, logger, mod
             self_verif_templates.extend(self_verif_templates_ner_tag)
         logger.info("Here is an example of a {} tag prompt :\n{}".format(ner_tag, first_prompts[-1]))
     
-    logger.info("Getting last line lengths...")
-    #line_lengths = [len(tokenizer.encode(prompt.split('\n')[-2])) for prompt in first_prompts]
-    #line_lengths = [len(tokenizer.encode(text)) for text in [example['text'] for example in testing_data]] if testing_data is not None else [len(tokenizer.encode(text)) for text in [example['text'] for example in training_data]]
+    reference = testing_data if testing_data is not None else training_data
     newline_token = tokenizer.encode('\n', add_special_tokens=False)[0]
+    eos_token = tokenizer.eos_token_id
+    yes_no = get_yes_no_words(keywords=kwargs['keywords'])
+    yes_tok = tokenizer.encode(yes_no[0],add_special_tokens=False)[0]
+    no_tok = tokenizer.encode(yes_no[1],add_special_tokens=False)[0]
+    sticked = True
+    begin_tag_toks = tokenizer.encode("@@",add_special_tokens=False)
+    if sticked:
+        end_tag_toks = tokenizer.encode('@##',add_special_tokens=False)[1:]
+    else :
+        end_tag_toks = tokenizer.encode("##",add_special_tokens=False)
+    if control:
+        entries = tokenizer([example['text'].strip()+'\n' for example in reference], add_special_tokens=False).input_ids
 
+    
     device = "cuda" if torch.cuda.is_available() else "cpu"
     logger.info("Generating...")
     outputs = []
-    reference = testing_data if testing_data is not None else training_data
     predictions = [
                     {
                         'doc_id': example['doc_id'],
@@ -120,7 +144,7 @@ def bloom_predict(training_data, testing_data, ner_tags, model_name, logger, mod
                     for example in reference
                 ]
     for i in tqdm(range(len(first_prompts))):
-        prompt = get_prompt_for_model(model_name, first_prompts[i])
+        prompt = get_prompt_for_model(model_name, [first_prompts[i]])[0]
         input_ids = tokenizer(prompt, padding=True, return_tensors="pt").input_ids
         input_ids = input_ids.to(device)
         if not control:
@@ -129,25 +153,22 @@ def bloom_predict(training_data, testing_data, ner_tags, model_name, logger, mod
             output = output[:,len(input_ids[0]):]
             output = tokenizer.decode(output[0], skip_special_tokens=True, skip_spaces_between_tokens=False)                
         else:
-            entry = tokenizer.encode(first_prompts[i].split('\n')[-2].split(':',2)[1].strip()+'\n', add_special_tokens=False)
-            sticked = True
-            
-            begin_tag_toks = tokenizer.encode("@@",add_special_tokens=False)
-            if sticked:
-                end_tag_toks = tokenizer.encode('@##',add_special_tokens=False)[1:]
-            else :
-                end_tag_toks = tokenizer.encode("##",add_special_tokens=False)
+            entry = entries[i%len(reference)]
+
             nb_open_entites = 0
-            while input_ids[0,-1]!=tokenizer.eos_token_id and len(entry)>1:
+            all_generated_ids = []
+            num_new_tokens = 0
+            while input_ids[0,-1] not in [newline_token, eos_token] and len(entry)>1 and num_new_tokens<512:
                 output = model.generate(input_ids,max_new_tokens=1,output_scores=True, return_dict_in_generate=True, **model_kwargs)
                 scores = output.scores
                 sequences = output.sequences
                 next_entry_id = entry[0]
-                allowed_tokens = [next_entry_id,begin_tag_toks[0]]
-                if nb_open_entites>0:
-                    allowed_tokens.append(end_tag_toks[0])
-                next_scores = {k:v for k,v in zip(allowed_tokens, scores[0][0][[allowed_tokens]])}
-                generated_id = max(next_scores, key=next_scores.get)
+                if nb_open_entites<=0:
+                    allowed_tokens = torch.tensor([next_entry_id, begin_tag_toks[0]]).to(device)
+                else:
+                    allowed_tokens = torch.tensor([next_entry_id, begin_tag_toks[0], end_tag_toks[0]]).to(device)
+                next_scores = scores[0][0][[allowed_tokens]]
+                generated_id = allowed_tokens[torch.argmax(next_scores)]
                 if generated_id==next_entry_id:
                     entry = entry[1:]
                     all_generated_ids = [generated_id]
@@ -157,9 +178,9 @@ def bloom_predict(training_data, testing_data, ner_tags, model_name, logger, mod
                     if sticked:
                         entry = tokenizer.encode("@"+tokenizer.decode(entry), add_special_tokens=False)[1:]
                 else:
-                    assert generated_id==end_tag_toks[0], generated_id
                     nb_open_entites-=1
                     all_generated_ids = end_tag_toks
+                num_new_tokens+=len(all_generated_ids)
 
                 input_ids = torch.cat([input_ids,torch.tensor(all_generated_ids).unsqueeze(0).to(device)], dim=1)
 
@@ -180,25 +201,17 @@ def bloom_predict(training_data, testing_data, ner_tags, model_name, logger, mod
             for ent_idx, (begin, end) in enumerate(get_indices(reference[i%len(reference)]['text'], output, begin_tag, end_tag))
         ])
         if self_verification:
-            yes_no = get_yes_no_words(keywords=kwargs['keywords'])
-            yes_tok = tokenizer.encode(yes_no[0],add_special_tokens=False)[0]
-            no_tok = tokenizer.encode(yes_no[1],add_special_tokens=False)[0]
-            allowed_tokens = [yes_tok, no_tok]
             verified_entities = []
             for pred in predictions[i%len(reference)]['entities']:
                 if pred['label']!=ner_tags[i//len(reference)]:
                     verified_entities.append(pred)
                     continue
                 verification_prompt = self_verif_templates[i].format(word=pred['text'])
-                # print(verification_prompt)
-                verification_prompt = get_prompt_for_model(model_name, verification_prompt)
+                verification_prompt = get_prompt_for_model(model_name, [verification_prompt])[0]
                 input_ids = tokenizer(verification_prompt, padding=True, return_tensors="pt").input_ids
                 input_ids = input_ids.to(device)
                 answer = model.generate(input_ids, max_new_tokens=1, output_scores=True, return_dict_in_generate=True)
-                scores = answer.scores[0][0][allowed_tokens]
-                # print('---')
-                # print(tokenizer.decode(answer.sequences[-1], skip_special_tokens=True))
-                # print('====')
+                scores = answer.scores[0][0][[yes_tok, no_tok]]
                 if scores[0]>scores[1]:
                     verified_entities.append(pred)
             predictions[i%len(reference)]['entities'] = verified_entities
