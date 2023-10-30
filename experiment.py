6e57dca45c61e3cfa2ed64ea0bc28d12d2d77b03
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-10-30 17:47:47 +0100

    conll2002 support

--- experiment.py
+++ experiment.py
@@ -207,16 +207,17 @@ prompt_keywords = {
         "no": "Non",
     }
 }
-
 ner_tags_by_dataset = {
     "WikiNER" : ["PER", "LOC", "ORG"],
     "conll2003" : ["PER", "LOC", "ORG"],
+    "conll2002" : ["PER", "LOC", "ORG"],
     "quaero" : ["ANAT", "CHEM", "DEVI", "DISO", "GEOG", "LIVB", "OBJC", "PHEN", "PHYS", "PROC"],
     "n2c2" : ["ACTI", "ANAT", "CHEM", "CONC", "DEVI", "DISO", "LIVB", "OBJC", "PHEN", "PHYS", "PROC"],
 }
 colnames_by_hf_dataset = {
     "WikiNER" : ("id", "words", "ner_tags"),
     "conll2003" : ("id", "tokens", "ner_tags"),
+    "conll2002" : ("id", "tokens", "ner_tags"),
 }
 tag_map_by_hf_dataset = {
     "WikiNER" : {
@@ -237,6 +238,19 @@ tag_map_by_hf_dataset = {
         7: "O",
         8: "O",
     },
+    #"O", "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "B-MISC", "I-MISC",
+    "conll2002" : {
+        0: "O",
+        1: "PER",
+        2: "PER",
+        3: "ORG",
+        4: "ORG",
+        5: "LOC",
+        6: "LOC",
+        7: "O",
+        8: "O",
+    },
+
 }
 
 def get_if_key_in_x(dict, x):
