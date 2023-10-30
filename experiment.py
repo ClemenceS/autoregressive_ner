7692e1d212d1b43348b5019b5c6a10ea4742b575
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-10-30 17:06:47 +0100

    res dict, n2c2 and sentcize

--- experiment.py
+++ experiment.py
@@ -4,11 +4,14 @@ import numpy as np
 import argparse
 import logging
 import random
+import json
 
-from predict import predict_for_dataset
+from predict import predict_for_dataset, MODEL_INSTRUCTION_TEMPLATES
 from nlstruct import BRATDataset, HuggingfaceNERDataset
 from nlstruct.metrics import MetricsCollection
 from nlstruct.registry import get_instance
+from nlstruct.data_utils import sentencize
+
 
 args = argparse.ArgumentParser()
 args.add_argument("--dataset_name", type=str, help="dataset name")
@@ -40,7 +43,6 @@ logger = logging.getLogger("experiment")
 #random deals with choosing the few-shot examples, so we want that fixed
 random.seed(args.random_seed)
 
-#assert dataset_name is not None
 assert args.dataset_name is not None
 
 prompt_keywords = {
@@ -62,7 +64,8 @@ prompt_keywords = {
             "OBJC" : "non-medical objects",
             "PHEN" : "physiolocal phenomema",
             "PHYS" : "human physiology",
-
+            "ACTI" : "activities",
+            "CONC" : "concepts",
             },
         'ner_tags' : {
             'PER' : "a person's name",
@@ -79,6 +82,8 @@ prompt_keywords = {
             "OBJC" : "a non-medical object",
             "PHEN" : "a physiolocal phenomemon",
             "PHYS" : "a human physiology",
+            "ACTI" : "an activity",
+            "CONC" : "a concept",
             },
         'ner_tags_description' : {
             'PER' : "These are words that refer to the name of a real or fictional person.",
@@ -95,6 +100,8 @@ prompt_keywords = {
             "OBJC" : "These are words that refer to an object that is not necessarily medical.",
             "PHEN" : "These are words that refer to a physiolocal phenomemon or a physiolocal function.",
             "PHYS" : "These are words that refer to a human physiology.",
+            "ACTI" : "These are words that refer to an activity.",
+            "CONC" : "These are words that refer to a medical concept.",
             },
         'input_intro' : "Input: ",
         'output_intro' : "Output: ",
@@ -205,6 +212,7 @@ ner_tags_by_dataset = {
     "WikiNER" : ["PER", "LOC", "ORG"],
     "conll2003" : ["PER", "LOC", "ORG"],
     "quaero" : ["ANAT", "CHEM", "DEVI", "DISO", "GEOG", "LIVB", "OBJC", "PHEN", "PHYS", "PROC"],
+    "n2c2" : ["ACTI", "ANAT", "CHEM", "CONC", "DEVI", "DISO", "LIVB", "OBJC", "PHEN", "PHYS", "PROC"],
 }
 colnames_by_hf_dataset = {
     "WikiNER" : ("id", "words", "ner_tags"),
@@ -244,6 +252,13 @@ try:
         ner_tags_colname=ner_tags_colname,
         load_from_disk=args.load_dataset_from_disk,
     )
+    #This is not supposed to be here, but WikiNER is a mess for now and I have no time to fix it
+    if args.dataset_name.endswith("WikiNER/en"):
+        dataset.train_data = [e for e in dataset.train_data if e['doc_id'].startswith('en')]
+    elif args.dataset_name.endswith("WikiNER/fr"):
+        dataset.train_data = [e for e in dataset.train_data if e['doc_id'].startswith('fr')]
+    elif args.dataset_name.endswith("WikiNER/es"):
+        dataset.train_data = [e for e in dataset.train_data if e['doc_id'].startswith('es')]
 except:
     dataset = BRATDataset(
         train= f"{args.dataset_name}/train",
@@ -252,43 +267,73 @@ except:
     )
 ner_tags = get_if_key_in_x(ner_tags_by_dataset, args.dataset_name)
 
+traindev_dataset = []
+for e in dataset.train_data:
+    sentences = sentencize(e, reg_split=r"(?<=[.|\s])(?:\s+)(?=[A-Z])", entity_overlap="split")
+    traindev_dataset.extend([s for s in sentences if len(s['text']) < 512])
+test_dataset = []
+for e in dataset.test_data:
+    sentences = sentencize(e, reg_split=r"(?<=[.|\s])(?:\s+)(?=[A-Z])", entity_overlap="split")
+    test_dataset.extend([s for s in sentences if len(s['text']) < 512])
 
-traindev_dataset = [e for e in dataset.train_data if len(e['text']) < 512]
-test_dataset = [e for e in dataset.test_data if len(e['text']) < 512]
+# traindev_dataset = list(sentencize(dataset.train_data, reg_split=r"(?<=[.|\s])(?:\s+)(?=[A-Z])", entity_overlap="split"))
+# test_dataset = list(sentencize(dataset.test_data, reg_split=r"(?<=[.|\s])(?:\s+)(?=[A-Z])", entity_overlap="split"))
+
+# traindev_dataset = [e for e in dataset.train_data if len(e['text']) < 512]
+# test_dataset = [e for e in dataset.test_data if len(e['text']) < 512]
 
 folder_name = 'results'
-os.makedirs(folder_name, exist_ok=True)
+#get script directory
+script_dir = os.path.dirname(__file__)
+#make the results folder if it doesn't exist
+os.makedirs(os.path.join(script_dir, folder_name), exist_ok=True)
 
 #np random deals with choosing the traindev dataset
 np.random.seed(args.partition_seed)
 traindev_dataset_this_seed = [traindev_dataset[i] for i in np.random.choice(len(traindev_dataset), size=args.training_size, replace=False)]
 
+res_dict = {}
 time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 last_two_dirs = '-'.join(args.dataset_name.split('/')[-2:])
 model_base_name = os.path.basename(args.model_name)
-logfile = open(folder_name+f'/log_{last_two_dirs}_{model_base_name}_{args.random_seed}_{time_str}.txt', 'w')
-logfile.write('dataset_name: '+last_two_dirs+'\n')
-logfile.write('begin_tag: '+args.begin_tag+'\n')
-logfile.write('end_tag: '+args.end_tag+'\n')
-logfile.write('n_few_shot: '+str(args.n_few_shot)+'\n')
-logfile.write('model_name: '+args.model_name+'\n')
-logfile.write('criterion: '+args.criterion+'\n')
-logfile.write('prompt_dict: '+args.prompt_dict+'\n')
-logfile.write('training_size: '+str(args.training_size)+'\n')
-logfile.write('partition_seed: '+str(args.partition_seed)+'\n')
-logfile.write('random_seed: '+str(args.random_seed)+'\n')
-logfile.write('control: '+str(args.control)+'\n')
-logfile.write('num_beams: '+str(args.num_beams)+'\n')
-logfile.write('self verification: '+str(args.self_verification)+'\n')
-# logfile.write('example prompt: \n'+prompts[0]+'\n')
-# logfile.write('self_verif_template: \n'+self_verif_template+'\n')
+# logfile = open(folder_name+f'/log_{last_two_dirs}_{model_base_name}_{args.random_seed}_{time_str}.txt', 'w')
+# logfile.write('dataset_name: '+last_two_dirs+'\n')
+# logfile.write('begin_tag: '+args.begin_tag+'\n')
+# logfile.write('end_tag: '+args.end_tag+'\n')
+# logfile.write('n_few_shot: '+str(args.n_few_shot)+'\n')
+# logfile.write('model_name: '+args.model_name+'\n')
+# logfile.write('criterion: '+args.criterion+'\n')
+# logfile.write('prompt_dict: '+args.prompt_dict+'\n')
+# logfile.write('training_size: '+str(args.training_size)+'\n')
+# logfile.write('partition_seed: '+str(args.partition_seed)+'\n')
+# logfile.write('random_seed: '+str(args.random_seed)+'\n')
+# logfile.write('control: '+str(args.control)+'\n')
+# logfile.write('num_beams: '+str(args.num_beams)+'\n')
+# logfile.write('self verification: '+str(args.self_verification)+'\n')
+res_dict['dataset_name'] = last_two_dirs
+res_dict['begin_tag'] = args.begin_tag
+res_dict['end_tag'] = args.end_tag
+res_dict['n_few_shot'] = args.n_few_shot
+res_dict['model_name'] = args.model_name
+res_dict['criterion'] = args.criterion
+res_dict['prompt_dict'] = args.prompt_dict
+res_dict['training_size'] = args.training_size
+res_dict['partition_seed'] = args.partition_seed
+res_dict['random_seed'] = args.random_seed
+res_dict['control'] = args.control
+res_dict['num_beams'] = args.num_beams
+res_dict['self_verification'] = args.self_verification
 if args.do_sample:
-    logfile.write('top_p: '+str(args.top_p)+'\n')
-    logfile.write('top_k: '+str(args.top_k)+'\n')
-    logfile.write('temperature: '+str(args.temperature)+'\n')
+    # logfile.write('top_p: '+str(args.top_p)+'\n')
+    # logfile.write('top_k: '+str(args.top_k)+'\n')
+    # logfile.write('temperature: '+str(args.temperature)+'\n')
+    res_dict['top_p'] = args.top_p
+    res_dict['top_k'] = args.top_k
+    res_dict['temperature'] = args.temperature
 else:
-    logfile.write('greedy'+'\n')
-logfile.write('='*50+'\n')
+    # logfile.write('greedy'+'\n')
+    res_dict['greedy'] = True
+# logfile.write('='*50+'\n')
 
 model_kwargs = {
     "num_beams": args.num_beams,
@@ -304,6 +349,14 @@ else:
     model_kwargs.update({
         "do_sample": False,
     })
+
+res_dict['prompt'] = prompt_keywords[args.prompt_dict]
+res_dict['chat_template'] = MODEL_INSTRUCTION_TEMPLATES[args.model_name] if args.model_name in MODEL_INSTRUCTION_TEMPLATES else ""
+res_dict['ner_tags'] = ner_tags
+res_dict['first_sentence'] = traindev_dataset_this_seed[0]['text']
+res_dict['last_sentence'] = traindev_dataset_this_seed[-1]['text']
+res_dict['test_on_test_set'] = args.test_on_test_set
+
 textual_outputs, predicted_dataset = predict_for_dataset(
     training_data=traindev_dataset_this_seed,
     testing_data=test_dataset if args.test_on_test_set else None,
@@ -336,16 +389,35 @@ for metric_name, metric in metrics.items():
     s_metrics+='ALL    tp: '+str(int(metric_dict['tp'].item()))+'    precision: '+str(round(metric_dict['precision'].item(), 3))+'    recall: '+str(round(metric_dict['recall'].item(), 3))+'    f1: '+str(round(metric_dict['f1'].item(), 3))+'\n'
     for tag in ner_tags:
         s_metrics+=tag+'    tp: '+str(int(metric_dict[tag+'_tp'].item()))+'    precision: '+str(round(metric_dict[tag+'_precision'].item(), 3))+'    recall: '+str(round(metric_dict[tag+'_recall'].item(), 3))+'    f1: '+str(round(metric_dict[tag+'_f1'].item(), 3))+'\n'
-logfile.write(s_metrics)
+# logfile.write(s_metrics)
 print(s_metrics)
+#convert all tensors to python types
+for k,v in metric_dict.items():
+    metric_dict[k] = round(v.item(), 3)
+res_dict['metrics'] = metric_dict
 
+full_preds = ""
 for i, (o, pred, gold) in enumerate(zip(textual_outputs, predicted_dataset, test_dataset if args.test_on_test_set else traindev_dataset_this_seed)):
-        logfile.write('='*50+'\n')
-        logfile.write('input: '+pred['text']+'\n')
+        # logfile.write('='*50+'\n')
+        # logfile.write('input: '+pred['text']+'\n')
+        full_preds += '='*50+'\n'
+        full_preds += 'input: '+pred['text']+'\n'
         for j,tag in enumerate(ner_tags):
-            logfile.write('-'*50+'\n')
-            logfile.write(tag+' output: '+textual_outputs[j*len(predicted_dataset)+i]+'\n')
-            logfile.write('final: '+str([p['text'] for p in pred['entities'] if p['label']==tag])+'\n')
-            logfile.write('gold: '+str([g['text'] for g in gold['entities'] if g['label']==tag])+'\n')
+            # logfile.write('-'*50+'\n')
+            # logfile.write(tag+' output: '+textual_outputs[j*len(predicted_dataset)+i]+'\n')
+            # logfile.write('final: '+str([p['text'] for p in pred['entities'] if p['label']==tag])+'\n')
+            # logfile.write('gold: '+str([g['text'] for g in gold['entities'] if g['label']==tag])+'\n')
+            full_preds += '-'*50+'\n'
+            full_preds += tag+' output: '+textual_outputs[j*len(predicted_dataset)+i]+'\n'
+            full_preds += 'final: '+str([p['text'] for p in pred['entities'] if p['label']==tag])+'\n'
+            full_preds += 'gold: '+str([g['text'] for g in gold['entities'] if g['label']==tag])+'\n'
 
-logfile.close()
+full_preds_path = os.path.join(script_dir, folder_name)+f'/full_preds_{last_two_dirs}_{model_base_name}_{args.random_seed}_{time_str}.txt'
+res_dict['full_preds_path'] = full_preds_path
+with open(full_preds_path, 'w') as f:
+    f.write(full_preds)
+
+res_dict_path = os.path.join(script_dir, folder_name)+f'/res_dict_{last_two_dirs}_{model_base_name}_{args.random_seed}_{time_str}.json'
+with open(res_dict_path, 'w') as f:
+    json.dump(res_dict, f)
+# logfile.close()
