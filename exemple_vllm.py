bb29a7bd51486ca8ab34e926385ec0ba596a6756
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-11-09 13:06:44 +0100

    Update model path and tensor parallel size

--- exemple_vllm.py
+++ exemple_vllm.py
@@ -1,17 +1,16 @@
 from vllm import LLM, SamplingParams
-import torch
-import os
 
-model_path = os.environ['DSDIR']+'/HuggingFace_Models/' + 'bloom-7b1'
+# model_path = os.environ['DSDIR']+'/HuggingFace_Models/' + 'bloom-7b1'
+model_path ="/gpfswork/rech/lak/utb11pp/models/lmsys/vicuna-13b-v1.5"
 
 prompts = [
     "Dear citizens,",
     "The capital of France is",
 ]
 print(prompts)
-sampling_params = SamplingParams(temperature=0.8)
+sampling_params = SamplingParams()
 
-llm = LLM(model=model_path, dtype="bfloat16", tensor_parallel_size=8) # tensor parallel size = number of gpus
+llm = LLM(model=model_path, dtype="bfloat16", tensor_parallel_size=2) # tensor parallel size = number of gpus
 print("model loaded")
 outputs = llm.generate(prompts, sampling_params)
 
