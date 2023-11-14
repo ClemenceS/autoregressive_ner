a11dbb9dc550626c52fa98ee5e434221a0dd7d0e
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-11-14 18:11:59 +0100

    adapt to work without jz access

--- scatterplot_results.py
+++ scatterplot_results.py
@@ -8,14 +8,18 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-l', '--local', action='store_true')
+parser.add_argument('-s', '--segur', action='store_true')
 args = parser.parse_args()
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 if not args.local:
     print('Downloading results...')
-    cmd1='rsync -r jean-zay.idris.fr:/gpfswork/rech/lak/utb11pp/autoregressive_ner/results/ {}/results/'.format(script_dir)
-    cmd2='rsync -r slurm-int:/mnt/beegfs/home/naguib/autoregressive_ner/results/ {}/results/'.format(script_dir)
+    if args.segur:
+        cmd1='rsync -r segur.limsi.fr:~/autoregressive_ner/results/ {}/results/'.format(script_dir)    
+    else:
+        cmd1='rsync -r jean-zay.idris.fr:/gpfswork/rech/lak/utb11pp/autoregressive_ner/results/ {}/results/'.format(script_dir)
+    cmd2='rsync -r slurm.lab-ia.fr:/mnt/beegfs/home/naguib/autoregressive_ner/results/ {}/results/'.format(script_dir)
     os.system(cmd1)
     os.system(cmd2)
     print('Done.')
