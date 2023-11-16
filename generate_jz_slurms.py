b4180dfcaf75275d1852d3ad048594ffc8c12f0c
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-11-16 11:14:27 +0100

    jz slurms

--- generate_jz_slurms.py
+++ generate_jz_slurms.py
@@ -3,6 +3,7 @@ models = {
     "/gpfswork/rech/lak/utb11pp/models/mistralai/Mistral-7B-v0.1" : "mistral",
     "/gpfswork/rech/lak/utb11pp/models/lmsys/vicuna-7b-v1.5" : "vic7",
     "/gpfswork/rech/lak/utb11pp/models/lmsys/vicuna-13b-v1.5" : "vic13",
+    "/gpfswork/rech/lak/utb11pp/models/stanford-crfm/BioMedLM": "biomedlm",
     #common models
     "/gpfsdswork/dataset/HuggingFace_Models/bigscience/bloom-7b1": "bloom7",
     "/gpfsdswork/dataset/HuggingFace_Models/bigscience/bloomz-7b1": "bloomz7",
@@ -11,6 +12,7 @@ models = {
     "/gpfsdswork/dataset/HuggingFace_Models/tiiuae/falcon-7b": "falcon7",
     "/gpfsdswork/dataset/HuggingFace_Models/tiiuae/falcon-40b": "falcon40",
     "/gpfsdswork/dataset/HuggingFace_Models/bofenghuang/vigogne-2-13b-instruct/": "vigogne13",
+    "/gpfsdswork/dataset/HuggingFace_Models/EleutherAI/gpt-j-6B": "gptj6",
 }
 datasets = {
     #local datasets
@@ -21,6 +23,9 @@ datasets = {
     "/gpfswork/rech/lak/utb11pp/data/mnaguib/WikiNER/en": "wnen",
     "/gpfswork/rech/lak/utb11pp/data/mnaguib/WikiNER/fr": "wnfr",
     "/gpfswork/rech/lak/utb11pp/data/mnaguib/WikiNER/es": "wnes",
+    "/gpfswork/rech/lak/utb11pp/data/e3c_en": "e3cen",
+    "/gpfswork/rech/lak/utb11pp/data/e3c_fr": "e3cfr",
+    "/gpfswork/rech/lak/utb11pp/data/e3c_es": "e3ces",
 }
 fixed_header="""#!/bin/bash
 
@@ -38,17 +43,19 @@ fixed_header="""#!/bin/bash
 
 module purge
 module load llm
+
+model={model}
 """
 
-line = "python3 $WORK/autoregressive_ner/clm_experiment.py --model_name {model} --dataset_name {dataset}  --n_gpus 2 -d"
+line = "python3 $WORK/autoregressive_ner/clm_experiment.py --model_name $model --dataset_name {dataset}  --n_gpus 2 -d"
 
 def generate_slurm(model):
     model_short_name = models[model]
     with open(f"slurms_jz/{model_short_name}.slurm", "w") as f:
-        f.write(fixed_header.format(model_short_name=model_short_name))
+        f.write(fixed_header.format(model_short_name=model_short_name, model=model))
         f.write("\n")
         for dataset in datasets:
-            f.write(line.format(model=model, dataset=dataset))
+            f.write(line.format(dataset=dataset))
             f.write("\n")
 
 for model in models:
