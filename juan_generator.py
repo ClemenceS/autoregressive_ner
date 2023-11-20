9a9cdb1d5fb7af57ac597eabcf78d829f8dc8294
marconaguib <marco.naguib@universite-paris-saclay.fr>
2023-11-20 11:25:20 +0100

    spanish double "de"

--- juan_generator.py
+++ juan_generator.py
@@ -19,7 +19,10 @@ for (name_en, string_en), (name_es, string_es), (name_fr, string_fr) in zip(en_s
         (words if word else sentences).append(s)
     else:
         #string_en/es/fr are dicts of strings
-        for (ner_en, ner_string_en), (ner_es, ner_string_es), (ner_fr, ner_string_fr) in zip(string_en.items(), string_es.items(), string_fr.items()):
+        # for (ner_en, ner_string_en), (ner_es, ner_string_es), (ner_fr, ner_string_fr) in zip(string_en.items(), string_es.items(), string_fr.items()):
+        for ner_es, ner_string_es in string_es.items():
+            ner_string_en = string_en[ner_es]
+            ner_string_fr = string_fr[ner_es] if ner_es in string_fr else ""
             word = len(ner_string_en.split()) < 7
             s = f"==================== {len((words if word else sentences))+1} ====================\n"
             s += ner_string_en.strip() + "\n"
@@ -29,7 +32,7 @@ for (name_en, string_en), (name_es, string_es), (name_fr, string_fr) in zip(en_s
             s += ner_string_es.strip() + "\n"
             (words if word else sentences).append(s)
 
-for w in words:
-    print(w)
-for s in sentences:
-    print(s)
+with open("strings.txt", "w") as f:
+    f.write("\n".join(words))
+    f.write("\n")
+    f.write("\n".join(sentences))
